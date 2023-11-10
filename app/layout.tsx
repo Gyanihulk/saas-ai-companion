96705bf608aee6f54bdb75f3eea5a534618940a7
GyaniHulk <adamya@appfoster.com>
2023-11-10 19:54:08 +0530

    companion page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/theme-provider'
 import { cn } from '@/lib/utils'
+import { Toaster } from '@/components/ui/toaster'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -22,7 +23,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning >
       <body className={cn("bg-secondary",inter.className) }>
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+        
         {children}
+        <Toaster/>
           </ThemeProvider>
         </body>
     </html>
